c89a40077523505ee1e5c998f67c9816a53e38b0
Jonathan Bailon Segura <jonn59@gmail.com>
2026-02-28 19:43:32 -0600

    chore: actualizar datos de autoría en licencia y encabezados de archivos

--- app/DTOs/InstallmentPurchaseDTO.php
+++ app/DTOs/InstallmentPurchaseDTO.php
@@ -1,10 +1,12 @@
 <?php
 
 /**
- * Copyright (c) 2026 Jonathan
+ * Copyright (c) 2026 Jonathan Bailon Segura <jonn59@gmail.com>
  * Licensed under the Polyform Noncommercial License 1.0.0
  * See LICENSE file in the project root for full license information.
  */
+
+
 
 
 namespace App\DTOs;
